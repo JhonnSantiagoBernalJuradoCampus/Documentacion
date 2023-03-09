39d1acd675226e73e5b2c3df60a8e08c4e8bebc2
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:14:55 -0500

    feat: :sparkles: Operador mayor que

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Operadores de comparaciòn
-//Operador Desigualdad estricta (!==), Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.
+//Operador Mayor que (>), Devuelve true si el operando izquierdo es mayor que el operando derecho.
 num1 = 15;
-num2 = "15";
-boleano = num1 !== num2
+num2 = 30;
+boleano = num1 > num2;
 alert(boleano);
-//El resultado sera true, en este caso es true ya que los valores son estrictamente desiguales
+//El resultado sera false, en este caso es false ya 15 no es mayor que 30
