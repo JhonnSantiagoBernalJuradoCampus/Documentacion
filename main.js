cf64b2d41a08aa6c3042accd5a5e072a01573edd
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 08:29:22 -0500

    feat: :sparkles: Operador aritmetico, Metodo .sqrt

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 //Operadores Aritmeticos (libreria matematica)
-//Metodo Math.random(), La función Math.random() devuelve un número de coma flotante pseudo-aleatorio, comprendido en el rango de 0 a menor que 1 (es decir, incluido el 0 pero no el 1), con una distribución aproximadamente uniforme en este rango. Dicho rango puede ser ampliado hasta los valores deseados. 
-alert(Math.random())
-//El resultado es un numero entre 0 y 1 float
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-alert(getRandomInt(3));
-//De esta forma obtenemos un numero entre 0 y 2
+//Metodo Math.sqrt(), La función Math.sqrt() retorna la raíz cuadrada de un número, Sintaxis Math.sqrt(x), Valor de Retorno La raíz cuadrada del número proporcionado. Si los números son negativos, NaN es devuelto.
+alert(Math.sqrt(9))
+//El resultado es 3
+alert(Math.sqrt(-9))
+//El resultado es NaN
