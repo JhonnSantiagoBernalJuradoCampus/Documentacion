c42291d066a48bd9990e786f660b75be62fa8cca
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 08:36:46 -0500

    feat: :sparkles: Operadores aritmeticos, Metodo .ceil

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 //Operadores Aritmeticos (libreria matematica)
 //Metodo Math.ceil, Math.ceil() La función Math.ceil() devuelve el entero mayor o igual más próximo a un número dado.
 alert(Math.ceil(.95));
-//En este caso El resultado sera 1
+//En este caso El resultado sera 1 
