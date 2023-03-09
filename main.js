3ed70aec195dc6a096f26e044adb054ef6df0b99
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:18:06 -0500

    feat: :sparkles: Operador Menor que

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Operadores de comparaciòn
-//Operador Mayor o igual que (>=),Devuelve true si el operando izquierdo es mayor o igual que el operando derecho.
+//Operador Menor que (<), Devuelve true si el operando izquierdo es menor que el operando derecho.
 num1 = 15;
-num2 = 14;
-boleano = num1 >= num2;
+num2 = 15;
+boleano = num1 < num2;
 alert(boleano);
-//El resultado sera true, en este caso es true ya que 15 es mayor que 14
+//El resultado sera false, en este caso es false ya que 15 no es menor que 15
