3172c18551c9d64dede3d7562bd907ac234d2893
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:01:34 -0500

    feat: :sparkles: Operador Positivo unario

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //Operadores aritmeticos
 //Operador de aritmetico
-//Operador de Negación unaria (-), Operador unario. Devuelve la negación de su operando.
-num1 = 10;
-num1 = -num1
+//Operador Positivo unario (+), Operador unario. Intenta convertir el operando en un número, si aún no lo es.
+num1 = "10";
+num1 = +num1
 alert(num1);
-//El resultado sera -10 
+//El resultado sera 10 
 
