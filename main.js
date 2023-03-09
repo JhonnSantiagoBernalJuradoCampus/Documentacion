0fc166dfd848ef3a1785ff7f43b0367037e4b63e
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 08:42:06 -0500

    feat: :sparkles: Operadores aritmeticos, Metodo .round

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 //Operadores Aritmeticos (libreria matematica)
-//Metodo Math.floor(), La siguiente función devuelve el valor entero redondeado más bajo de la variable x:
+//Metodo Math.round(), Devuelve el valor del número dado redondeado al entero más cercano.
 alert(Math.floor(15.65))
-//El resultado es 15 ya que es el entero anterior
+//El resultado es 16 ya que es el entero mas cercano
