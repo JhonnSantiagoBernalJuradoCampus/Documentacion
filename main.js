cdc00747b22587cbb681c6d649ef42c3a179ccfb
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:12:23 -0500

    feat: :sparkles: Operador Estrictamente igual

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Operadores de comparaciòn
-//Operador No es igual (!=), Devuelve true si los operandos no son iguales.
+//Operador Estrictamente igual (===), Devuelve true si los operandos son iguales y del mismo tipo. Consulta también Object.is y similitud en JS.
 num1 = 15;
 num2 = "15";
-boleano = num1 != num2
+boleano = num1 === num2
 alert(boleano);
-//El resultado sera false, en este caso es false ya que los valores no son estrictamente falsos
+//El resultado sera false, en este caso es false ya que los valores no son estrictamente iguales
