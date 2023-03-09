530e109f2c3b7fb00bd3ec62647803bc93e525fd
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:13:16 -0500

    feat: :sparkles: Operador Estrictamente desigual

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Operadores de comparaciòn
-//Operador Estrictamente igual (===), Devuelve true si los operandos son iguales y del mismo tipo. Consulta también Object.is y similitud en JS.
+//Operador Desigualdad estricta (!==), Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.
 num1 = 15;
 num2 = "15";
-boleano = num1 === num2
+boleano = num1 !== num2
 alert(boleano);
-//El resultado sera false, en este caso es false ya que los valores no son estrictamente iguales
+//El resultado sera true, en este caso es true ya que los valores son estrictamente desiguales
