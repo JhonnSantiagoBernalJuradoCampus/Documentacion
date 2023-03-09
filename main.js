e444444d156e3d295444abcf2513f18838c0ac60
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:26:53 -0500

    feat: :sparkles: Operador Or

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //Operadores logicos
-//Operador AND Lógico (&&), uso expr1 && expr2, Devuelve expr1 si se puede convertir a false; de lo contrario, devuelve expr2. Por lo tanto, cuando se usa con valores booleanos, && devuelve true si ambos operandos son true; de lo contrario, devuelve false.
+//Operador OR lógico (||), uso expr1 || expr2, uso Devuelve expr1 si se puede convertir a true; de lo contrario, devuelve expr2. Por lo tanto, cuando se usa con valores booleanos, || devuelve true si alguno de los operandos es true; si ambos son falsos, devuelve false.
 num1 = 15;
 num2 = 17;
 num3 = 23;
 boleano = num2 > num1;
-boleano2 = num2 < num3;
-alert(boleano && boleano2);
-//El resultado sera true ya que las dos condiciones son verdaderas
+boleano2 = num2 > num3;
+alert(boleano || boleano2);
+//El resultado sera true ya almenos uno de las condiciones es true
