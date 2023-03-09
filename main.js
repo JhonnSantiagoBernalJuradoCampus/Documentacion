164547fc9984967b2a48008a1ed85eccff374476
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:45:45 -0500

    feat: :sparkles: Operadores Aritmeticos, Propiedad Math.PI

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-//Operadores logicos
-//Operador NOT lógico (!), uso !expr, Devuelve false si su único operando se puede convertir a true; de lo contrario, devuelve true.
-num1 = 17;
-num2 = 23;
-boleano = num1 < num2;
-not = !boleano
-alert(not);
-//El resultado es false ya que la condicion se puede volver false
+//Operadores Aritmeticos (libreria matematica)
+//Propiedad Math.PI La propiedad Math.PI representa la relacion entre la longitud de la circunferencia de un circulo y su diametro, la cual es aproximadamente 3.14159.
+//La siguiente función utiliza Math.PI para calcular la longitud de la circunferencia de un circulo mediante el radio del circulo proporcionado como parametro.
+function calculaCircunferencia(radio) {
+    resultado = 2 * Math.PI * radio;
+    alert(resultado)
+  }
+calculaCircunferencia(1);
+//El resultado es 6.283185307179586
