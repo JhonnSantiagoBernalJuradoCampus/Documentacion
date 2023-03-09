aa54c9f0cdc52e3d6e2352d16a58b25fbcae6355
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 08:31:27 -0500

    feat: :sparkles:

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 //Operadores Aritmeticos (libreria matematica)
-//Metodo Math.sqrt(), La función Math.sqrt() retorna la raíz cuadrada de un número, Sintaxis Math.sqrt(x), Valor de Retorno La raíz cuadrada del número proporcionado. Si los números son negativos, NaN es devuelto.
-alert(Math.sqrt(9))
-//El resultado es 3
-alert(Math.sqrt(-9))
-//El resultado es NaN
+//Metodo Math.ceil, Math.ceil() La función Math.ceil() devuelve el entero mayor o igual más próximo a un número dado.
+alert(Math.ceil(.95));
+//En este caso El resultado sera 1
