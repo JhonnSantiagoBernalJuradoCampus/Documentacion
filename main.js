79888ef4f36c73a03b68da6853161fcd4ed075ae
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:08:26 -0500

    feat: :sparkles: Operadores de Comparacion, Operador Igual

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-//Operadores aritmeticos
-//Operador de aritmetico
-//Operador Operador de exponenciación (**), Calcula la base a la potencia de exponente, es decir, baseexponente
-num1 = 10;
-num2 = 2;
-num1 = num1**num2;
-alert(num1);
-//El resultado sera 100 (10**2= 100)
+//Operadores de comparaciòn
+//Operador Igual (==), Devuelve true si los operandos son iguales.
+num1 = 15;
+num2 = "15";
+boleano = num1 == num2
+alert(boleano);
+//El resultado sera true, este valor == no es estricto por lo cual no toma la diferencia de los tipos de datos
 
