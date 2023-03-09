da52ada3a379da6651fdde05998544aab4422cd1
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 06:58:29 -0500

    feat: :sparkles: Operador de negaciòn unaria

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //Operadores aritmeticos
 //Operador de aritmetico
-//Operador de Decremento (--), Le resta 1 al valor
+//Operador de Negación unaria (-), Operador unario. Devuelve la negación de su operando.
 num1 = 10;
---num1;
+num1 = -num1
 alert(num1);
-//El resultado serà 9 (10-1)= 9
+//El resultado sera -10 
 
