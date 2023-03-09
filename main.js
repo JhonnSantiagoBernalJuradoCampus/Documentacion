bc18ec9aba73109c049dbaf461604096e08af25f
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 07:10:32 -0500

    feat: :sparkles: Operador de comparacion, No es igual

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 //Operadores de comparaciòn
-//Operador Igual (==), Devuelve true si los operandos son iguales.
+//Operador No es igual (!=), Devuelve true si los operandos no son iguales.
 num1 = 15;
 num2 = "15";
-boleano = num1 == num2
+boleano = num1 != num2
 alert(boleano);
-//El resultado sera true, este valor == no es estricto por lo cual no toma la diferencia de los tipos de datos
-
+//El resultado sera false, en este caso es false ya que los valores no son estrictamente falsos
