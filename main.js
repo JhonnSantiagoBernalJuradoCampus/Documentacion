ac1c494a85a981b87f4b380b73bc7aef8b9fb842
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 06:49:05 -0500

    feat: :zap: Operadores aritmeticos, operador de residuo

---
+++ main.js
@@ -0,0 +1,8 @@
+//Operadores aritmeticos
+//Operador de aritmetico
+//Operador de Residuo (%), Operador binario. Devuelve el resto entero de dividir los dos operandos.
+let num1 = 15
+let num2 = 5
+alert(num1%num2)
+//En este caso el resultado es 0 ya que al dividir 15 entre 5 no sobra nada
+
