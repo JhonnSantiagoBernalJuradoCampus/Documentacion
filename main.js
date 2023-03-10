356047a7eb1c61334f93183a36ceb91e9a701c44
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 23:13:25 -0500

    feat: :sparkles: String, Metodo .codePointAt

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 //Propiedades de los string
-//Metodo .chartAt
-//El metodo .chartAt devuelve el caracter de la cadena en el indice que se indica
-
-
-let cadena = "Hola mundo";
-let indice = cadena.charAt(0)
-alert(indice)
-//El resultado es H ya que esta en la posicion 0 de la cadena
-
-
-let cantidad = cadena.length
-let indice2 = cadena.charAt(--cantidad)
-alert(indice2)
-//En este caso el resultado sera el ultimo caracter de la cadena de texto ya que primero pido la cantidad de datos en la cadena y despues le resto 1 que vendria siendo la ultima posicion de la cadena de texto
+//Metodo codePointAt()
+//El metodo .codePointAt() Devuelve el código ASCII en la posición index pasada por parámetro.
+let cadena = "Hola Mundo"
+alert(cadena.codePointAt(1))
+//El resultado es 111 ya que el codigo ASCII de la o es 111
