ea4c8caee2a8465ce4f38531d07c0982a7c1dd03
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 23:38:49 -0500

    feat: :sparkles: String, Metodo lastIndexOf

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 //Propiedades de los string
 //Metodo indexOf
-//El método indexOf() devuelve el índice, dentro del objeto String que realiza la llamada, de la primera ocurrencia del valor especificado, comenzando la búsqueda desde indiceDesde; o -1 si no se encuentra dicho valor.
-
-
+//El método lastIndexOf() devuelve la posicion (indice) en la que se encuentra el valorBusqueda, dentro del objeto String que realiza la llamada, de la última ocurrencia del valor especificado; o -1 si no se halla. La búsqueda se realiza empezando por el final de la cadena que realiza la llamada.
 let cadena = "Santiago Bernal";
-let posicion = cadena.indexOf("t");
-alert(posicion)
-//El resultado es 3 ya que se encuentra en ese indice el valor "t"
+let indice = cadena.lastIndexOf("a");
+alert(indice)
+//El resultado es 13 ya que toma la primera aparicion del valor a empezando desde el final de la cadena
