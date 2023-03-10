f79b6c386be3ee025563373ba4e875bb5c8ff612
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 23:46:47 -0500

    feat: :sparkles: String, Metodo search

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Propiedades de los string
-//Metodo indexOf
-//El método lastIndexOf() devuelve la posicion (indice) en la que se encuentra el valorBusqueda, dentro del objeto String que realiza la llamada, de la última ocurrencia del valor especificado; o -1 si no se halla. La búsqueda se realiza empezando por el final de la cadena que realiza la llamada.
-let cadena = "Santiago Bernal";
-let indice = cadena.lastIndexOf("a");
-alert(indice)
-//El resultado es 13 ya que toma la primera aparicion del valor a empezando desde el final de la cadena
+//Metodo search()
+//El metodo search() busca una cadena para un valor especificado y devuelve la posición del partido. El valor de búsqueda puede ser de cadena o una expresión regular. Este método devuelve -1 si no se encuentra ninguna coincidencia.
+let cadena = "Hola Mundo";
+let indice = cadena.search("Mundo");
+alert(indice);  
+//El resultado es 5 ya que el valor se encuentra en la posicion 5 de la cadena de texto
