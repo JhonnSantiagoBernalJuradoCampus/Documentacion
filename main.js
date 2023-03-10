97075a1d9363a6f7aab3a0aaf340684a56c7d527
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 21:05:31 -0500

    feat: :sparkles: String, Metodo .charAt

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 //Propiedades de los string
-//Propiedad .length, Devuelve el numero de caracteres de una cadena de texto
-let cadena = "Hola Mundo"
-cantidad = cadena.length
-alert(cantidad)
-//El resultado es 10 ya que ese es el numero de caracteres contando el espacio
+//Metodo .chartAt
+//El metodo .chartAt devuelve el caracter de la cadena en el indice que se indica
+
+
+let cadena = "Hola mundo";
+let indice = cadena.charAt(0)
+alert(indice)
+//El resultado es H ya que esta en la posicion 0 de la cadena
+
+
+let cantidad = cadena.length
+let indice2 = cadena.charAt(--cantidad)
+alert(indice2)
+//En este caso el resultado sera el ultimo caracter de la cadena de texto ya que primero pido la cantidad de datos en la cadena y despues le resto 1 que vendria siendo la ultima posicion de la cadena de texto
