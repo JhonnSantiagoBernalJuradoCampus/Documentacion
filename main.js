9aeca55a2d3c88e36966debc072904776f32acc7
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 23:23:46 -0500

    feat: :sparkles: String, Metodo fromCharCode

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 //Propiedades de los string
-//Metodo codePointAt()
-//El metodo .codePointAt() Devuelve el código ASCII en la posición index pasada por parámetro.
-let cadena = "Hola Mundo"
-alert(cadena.codePointAt(1))
-//El resultado es 111 ya que el codigo ASCII de la o es 111
+//Metodo fromCharCode()
+//El String.fromCharCode() método estático que devuelve una cadena creada mediante el uso de una secuencia de valores ASCII.
+alert(String.fromCharCode(65,66,67))
