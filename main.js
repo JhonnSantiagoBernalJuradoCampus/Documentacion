fee38b8f3ea9b27753f75b678417e22e5de4e7e2
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-12 17:45:53 -0500

    feat: :sparkles: String, Metodo match

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 //Propiedades de los string
-//Metodo localeCompare
-//El metodo compara dos cadenas en la configuración regional actual, devuelve en un criterio entre -1 y 1 (-1 para before 0 para equal y 1 para after)
-let cadena = "abcd";
-let cadena2 = "efgh";
-let criterio = cadena.localeCompare(cadena2);
-alert(criterio)
-//El resultado es -1 ya que va antes
+//Metodo match
+//El match() método busca una cadena para un partido contra una expresión regular, y devuelve los partidos, como un objeto Array. Leer más acerca de las expresiones regulares en nuestro Tutorial RegExp y nuestra referencia RegExp objeto . Nota: Si la expresión regular no incluye el modificador g (to perform a global search) , el match() método devolverá sólo la primera coincidencia en la cadena. Este método devuelve un valor nulo si no se encuentra ninguna coincidencia.
+
+let cadena = "Hola mundo. Campus es lo mejor del mundo:)";
+let resultado = cadena.match(/mundo/g);
+alert(resultado);
+
+//El resultado es mundo,mundo
+
+resultado = cadena.match(/mundo/);
+alert(resultado);
+
+//En este caso el rersultado es mundo ya que no pusimos la g (to perform a global search) por lo cual solo devuelve la primera vez que encuentra el dato en la cadena
