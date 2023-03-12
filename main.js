f5eb17d8850e2f6b91bdffd41574cad21ee49215
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-12 18:36:38 -0500

    feat: :sparkles: String, Metodo substring

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 //Propiedades de los string
-//Metodo slice
-//El metodo slice extrae el texto de una cadena y devuelve una nueva cadena. Los cambios en el texto de una cadena no afectan a la otra cadena.
-//slice extrae hasta, pero sin incluir finalTrozo. string.slice(1,4) extrae del segundo carácter hasta el cuarto carácter (caracteres con índice 1, 2 y 3).
-let cadena = "Hola Mundo, ¿Que tal tu día?";
-let resultado = cadena.slice(0,10)
-alert(resultado)
-//El resultado es Hola Mundo
+//Metodo substring
+//El metodo substring El substring() método devuelve un subconjunto de un objeto String.
+let cadena = "Hola mundo";
+let resultado = cadena.substring(0,4);
+alert(resultado);
+//El resultado es Hola
