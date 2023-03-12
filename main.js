26ab57b312781831472051b71f3c90afd329a00b
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-12 18:46:45 -0500

    feat: :sparkles: String, Metodo trim

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Propiedades de los string
-//Metodo toLowerCase
-//El método toUpperCase() devuelve el valor en mayusculas de la cadena que realiza la llamada.
-let cadena = "Hola Mundo Nuevo";
-let mayuscula = cadena.toUpperCase()
-alert(mayuscula)
+//Metodo trim
+//El método trim( ) elimina los espacios en blanco en ambos extremos del string. Los espacios en blanco en este contexto, son todos los caracteres sin contenido (espacio, tabulación, etc.) y todos los caracteres de nuevas lineas (LF,CR,etc.).
+let cadena = "           Hola           ";
+alert(cadena)
+let trim = cadena.trim();
+alert(trim)
