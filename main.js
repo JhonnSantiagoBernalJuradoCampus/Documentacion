6118b17b563b5d52e6cf2e2a0415e07fbb552bd6
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-12 17:55:08 -0500

    feat: :sparkles: String, Metodo matchAll

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 //Propiedades de los string
-//Metodo match
-//El match() método busca una cadena para un partido contra una expresión regular, y devuelve los partidos, como un objeto Array. Leer más acerca de las expresiones regulares en nuestro Tutorial RegExp y nuestra referencia RegExp objeto . Nota: Si la expresión regular no incluye el modificador g (to perform a global search) , el match() método devolverá sólo la primera coincidencia en la cadena. Este método devuelve un valor nulo si no se encuentra ninguna coincidencia.
-
-let cadena = "Hola mundo. Campus es lo mejor del mundo:)";
-let resultado = cadena.match(/mundo/g);
-alert(resultado);
-
-//El resultado es mundo,mundo
-
-resultado = cadena.match(/mundo/);
-alert(resultado);
-
-//En este caso el rersultado es mundo ya que no pusimos la g (to perform a global search) por lo cual solo devuelve la primera vez que encuentra el dato en la cadena
+//Metodo matchAll
+//El método matchAll()devuelve un iterador de todos los resultados que coinciden con una cadena contra una expresión regular,incluyendo los grupos de captura.
+let cadena = "Hola Mundo Mundo Mundo Mundo Mundo Mundo";
+let resultado = [...cadena.matchAll(/Mundo/g)];
+alert(resultado)
