c609c3c2a764f49c61c97d87bc15eea64ac3cef1
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-12 18:16:51 -0500

    feat: :sparkles: String, Metodo replace

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //Propiedades de los string
-//Metodo repeat
-//El método repeat() construye y devuelve una nueva cadena que contiene el número especificado de copias de la cadena en la cual fue llamada, concatenados.
-let cadena = "Hola mundo ";
-let repetir = cadena.repeat(3);
-alert(repetir);
+//Metodo replace
+//El método replace() devuelve una nueva cadena con algunas o todas las coincidencias de un patrón, siendo cada una de estas coincidencias reemplazadas por remplazo. El patrón puede ser una cadena o una RegExp, y el reemplazo puede ser una cadena o una función que será llamada para cada coincidencia. Si el patrón es una cadena, sólo la primera coincidencia será reemplazada.
+
+let nombres = ["Miguel", "Santiago"];
+let cadena = `Hola ${nombres[0]}, ¿Como vas?`;
+alert(cadena);
+let remplazar = cadena.replace(nombres[0], nombres[1]); 
+alert(remplazar);
