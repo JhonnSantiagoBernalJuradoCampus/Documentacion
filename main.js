55ec98229423c6d480f8fca6eb224f4e7056ae72
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-12 18:42:54 -0500

    feat: :sparkles: String, Metodo toLowerCase

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 //Propiedades de los string
-//Metodo split
-//El método split() divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas.
+//Metodo toLowerCase
+//El método toLowerCase() devuelve el valor en minúsculas de la cadena que realiza la llamada.
 let cadena = "Hola Mundo Nuevo";
-let split = cadena.split(" ");
-alert(split);
-//El resultado es Hola,Mundo,Nuevo
-//Lo que hace es separarlo por el parameto " "
+let minuscula = cadena.toLowerCase()
+alert(minuscula)
