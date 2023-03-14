87b45725c49cba9c43917d78cb9afb011994fb66
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-14 18:06:59 -0500

    feat: :sparkles: Objetos, Metodo Object.fromEntries

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-//Funciones
-//Funciones flecha
-const funcionT = function () {
-  return "Función tradicional.";
-};
+//Objetos
+//Metodo Object.fromEntries
+//El método Object.entries() devuelve una matriz de pares propios de una propiedad enumerable [key, value] de un objeto dado, en el mismo orden que es proporcionado por for...in (La diferencia es que un bucle for-in enumera las propiedades en la cadena de prototipos).
 
-const funcionF = () => {
-  return "Función flecha.";
-};
+let array = ["Dato #0","Dato #1","Dato #2","Dato #3"];
+console.log(Object.entries(array))
+
+//Este metodo lo que hace es devolver una matriz con su llave o indice y su valor
